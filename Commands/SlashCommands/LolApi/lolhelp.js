17b90827e7e44d0fcc539efb06c2ac15d95d246f
Jakub <jakub.wiecek@outlook.com>
2021-09-04 17:40:38 +0200

    edit commands description

--- Commands/SlashCommands/LolApi/lolhelp.js
+++ Commands/SlashCommands/LolApi/lolhelp.js
@@ -18,25 +18,25 @@ module.exports = {
         .addFields(
             {
                 name: "Historia meczu",
-                value: "Aby uzyskać informacje na temat statystyk z któregoś z ostatnich meczu należy użyć komendy: ```fix\n!h nr_meczu nick``` " +
+                value: "Aby uzyskać informacje na temat statystyk z któregoś z ostatnich meczu należy użyć komendy: ```fix\n/h nr_meczu nick``` " +
                     "Przykładowo komenda: ```/h 1 Patryk``` wyświetli statystyki z ostatniego meczu gracza Patryk."
 
             },
             {
                 name: "Mecz na żywo:",
-                value: "Aby uzyskać podstawowe informacje na temat swoich sojuszników i przeciwników w obecnie trwającym meczu należy użyć komendy: ```fix\n!l nick```" +
+                value: "Aby uzyskać podstawowe informacje na temat swoich sojuszników i przeciwników w obecnie trwającym meczu należy użyć komendy: ```fix\n/l nick```" +
                     "Przykładowo komenda: ```/l Patryk``` wyświetli trwający mecz gracza Patryk."
 
             },
             {
                 name: 'Informacje o koncie',
-                value: "Aby uzyskać informacje na temat konta należy użyć komendy: ```fix\n!a nick```"
+                value: "Aby uzyskać informacje na temat konta należy użyć komendy: ```fix\n/a nick```"
                     + "Przykładowo komenda: ```/a Patryk ``` wyświetli informacje o koncie gracza Patryk"
 
             },
             {
                 name: 'Informacje o koncie i maestrie bohaterów',
-                value: "Aby uzyskać informacje na temat najczęściej wybieranych postaciach należy użyć komendy: ```fix\n!m nick```"
+                value: "Aby uzyskać informacje na temat najczęściej wybieranych postaciach należy użyć komendy: ```fix\n/m nick```"
                     + "Przykładowo komenda: ```/m Patryk ``` wyświetli informacje o maestriach gracza Patryk"
 
             },
