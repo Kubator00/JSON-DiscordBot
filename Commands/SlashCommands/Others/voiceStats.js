311db73be0fae0c90f71ebe3c9200fc04665f5f7
Jakub <jakub.wiecek@outlook.com>
2021-11-22 14:19:59 +0100

    fix voiceStats

--- Commands/SlashCommands/Others/voiceStats.js
+++ Commands/SlashCommands/Others/voiceStats.js
@@ -2,9 +2,20 @@ const displayVoiceStats = require("../../../voiceStats/displayVoiceStats.js");
 
 module.exports = {
     name: 'statystyki',
-    description: "Wyświetla statystyki",
+    description: "Wyświetla ranking osób z najłuższą ilością czasu spędzonego na kanałach głosowych",
 
     async execute(msg) {
+        const roleName = "Zweryfikowany";
+        const roleId = (msg.guild.roles.cache.find(p => p.name === roleName)).id;
+        let hasRoles = 0;
+        for (userRole of msg.member._roles)
+            if (userRole == roleId)
+                hasRoles = 1;
+
+        if (hasRoles == 0 && msg.member.permissions.has('ADMINISTRATOR') != true) {
+            msg.followUp(`Aby użyć tej komendy musisz posiadać rolę "${roleName}".`)
+            return;
+        }
         msg.followUp('.');
         displayVoiceStats.send_time_voice(msg.channel);
     }
